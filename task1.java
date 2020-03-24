c8c705e879375b65ab37d7c597583431741c5162
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-03-24 19:22:50 +0200

    task Hello in stars v2

--- task1.java
+++ task1.java
@@ -6,12 +6,12 @@ public class task1 {
 String p1= "*    * ***** *     *      ****";
 String p2= "*    * *     *     *     *    *";
 String p3= "****** ***** *     *     *    *";
-String p4= "*    * *     *     *     *    *";
+//String p4= "*    * *     *     *     *    *";
 String p5= "*    * ***** ***** *****  ****";  
           System.out.println(p1);
           System.out.println(p2);                
           System.out.println(p3);                
-          System.out.println(p4);                
+          System.out.println(p2);                
           System.out.println(p5);                
     }
 }
