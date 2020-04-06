d9c774f9355966ce1c358a58be7fd5f0f4276273
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-04-06 23:12:22 +0300

    describe fur PetMaster

--- PetMaster.java
+++ PetMaster.java
@@ -1,9 +1,12 @@
 public class PetMaster {
+    //see with Pet.java
     public static void main (String[] args) {
         String petReaction;
         Pet myPet = new Pet();
         myPet.eat();
         petReaction = myPet.say("Njam");
+        // myPet.say("Njam Njam "); - not working
+        // myPet.say(); - not working
         System.out.println(petReaction);
         myPet.sleep();
     }
