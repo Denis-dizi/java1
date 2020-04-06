d055b7e5bc331ca24c9460d7858a2c6ebb88145a
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-04-06 20:41:22 +0300

    changed class pet1

--- pet1.java
+++ pet1.java
@@ -1,10 +1,24 @@
-/*p40/p46
-class
-declare variables fur class attributes
+/*p40-Eng/p46-Ru
+class;
+declare variables fur class attributes;
+does method should take arguments and return a value?;
 */
-class Pet {
+public class pet1 {
     int age;
     float weighr;
     float height;
     String color;
+
+    public void sleep (){
+        System.out.println(
+            "Good night, see you tomorrow");
+    }
+    public void eat (){
+        System.out.println(
+            "Hungry! Give me nachos!! ASAP!!!");
+    }
+    public String say(String aWord){
+        String petResponse = "OK" +aWord;
+            return petResponse;
+    }
 }
