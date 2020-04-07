5e70c363719f38c4152cc0ce3b300d29afc7c260
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-04-07 21:09:34 +0300

    practice: Car.java

---
+++ Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    //practice (p50/p57)
+    public void start (){
+        System.out.println(
+            "Started");
+    }
+    public void stop (){
+        System.out.println(
+            "Stoped");
+    }
+    int distance=0
+    public int drive (int howlong){
+        distance=howlong*60;
+        System.out.println("Have driven " + distance + " km");
+       // return distance;
+    }
+}
---
+++ CarOwner.java
@@ -0,0 +1,7 @@
+public class CarOwner {
+    //practice (p50/p57)
+    public static void main (int[] args) {
+        Car myCar = new Car();
+        myCar.drive(2);
+    }
+}
