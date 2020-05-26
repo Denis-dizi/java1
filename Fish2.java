f26d98b55339a0b0268b86e397fcec1bb3628ffb
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-05-26 22:36:10 +0300

    New class FishMaster2. Class Fish2 corrections.

--- Fish2.java
+++ Fish2.java
@@ -5,12 +5,11 @@ public class Fish2 extends Pet {
     // class Fish extends Pet {}
     int currentDepth=0;
     public int dive (int howDeep) {
-        currentDepth=currentDepth + howDeep;
+        currentDepth = currentDepth + howDeep;
         System.out.println("Diving for " + howDeep +" feet");
         System.out.println("I'm at " + currentDepth + " feet below see level");
-            }
         return currentDepth;
-    }
+    }}
 //dive() - method has an argument "howDeep".
 //howDeep - argument(параметр) tells how deep fish should go.
 //currentDepth - class variable will store and update depth every call method "dive".
---
+++ FishMaster2.java
@@ -0,0 +1,11 @@
+public class FishMaster2 {
+    //see with Fish2.java
+    public static void main (String[] args) {
+        Fish2 myFish = new Fish2();
+        myFish.dive(2);
+        myFish.dive(3);
+        myFish.sleep();
+    }
+}
+//Метод main() в классе FishMaster создает экземпляр объекта Fish и дважды вызывает его метод dive() с разными параметрами.
+//The method main() instantiates the object Fish and calls its method dive() twice with different arguments.
