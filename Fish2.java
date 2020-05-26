72305495afc5aa90a533243394e19ad78577f050
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-05-26 22:25:19 +0300

    new class Fish2

---
+++ Fish2.java
@@ -0,0 +1,16 @@
+public class Fish2 extends Pet {
+    //p47/p54-Ru
+    // Inheritance (наследование) (p45-Eng/p52-Ru)
+    // extends - keyword to inherit attributes from other class:
+    // class Fish extends Pet {}
+    int currentDepth=0;
+    public int dive (int howDeep) {
+        currentDepth=currentDepth + howDeep;
+        System.out.println("Diving for " + howDeep +" feet");
+        System.out.println("I'm at " + currentDepth + " feet below see level");
+            }
+        return currentDepth;
+    }
+//dive() - method has an argument "howDeep".
+//howDeep - argument(параметр) tells how deep fish should go.
+//currentDepth - class variable will store and update depth every call method "dive".
