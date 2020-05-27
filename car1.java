7193afc40fa9ec772e3953c1f5c6b955e6096860
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-05-27 16:26:30 +0300

    class Car1 got to work.

--- car1.java
+++ car1.java
@@ -1,7 +1,10 @@
 public class car1 {
-    public void start ();
-    public void stop ();
-    public int drive (int howlong);
-    int distance;
-    distance = howlong * 60;
+    public void start (){};
+    public void stop (){};
+    public int drive (int howlong){
+        int distance = 0;
+        distance = howlong * 60;
+        System.out.println("With speed 60mp/h car driven" + distance + "miles.");
+        return distance;
+    }   
 }
