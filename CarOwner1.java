75f91cddf4cbd2f8e09ee6cb0df7a1e3180debc8
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-05-27 18:28:13 +0300

    New class CarOwner1. Class Car1 modifed.

---
+++ CarOwner1.java
@@ -0,0 +1,8 @@
+public class CarOwner1 {
+    public static void main (String[] args) {
+        Car1 myCar1 = new Car1();
+        myCar1.start();
+        myCar1.stop();
+        System.out.println (myCar1.drive(2));
+    }
+}
--- car1.java
+++ car1.java
@@ -1,14 +1,14 @@
-public class car1 {
+public class Car1 {
     public void start (){
         System.out.println("The car is started.");
     };
     public void stop (){
         System.out.println("The car is stoped.");
     };
+    int distance = 0;
     public int drive (int howlong){
-        int distance = 0;
         distance = howlong * 60;
-        System.out.println("With speed 60mp/h car driven" + distance + "miles.");
+        System.out.println("With speed 60mp/h car driven " + distance + " miles.");
         return distance;
     }   
 }
