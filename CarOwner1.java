cac7da03a6931c1dfb15343942bfd0422dee98ec
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-05-27 18:37:16 +0300

    class CarOwner1 modified.

--- CarOwner1.java
+++ CarOwner1.java
@@ -3,6 +3,11 @@ public class CarOwner1 {
         Car1 myCar1 = new Car1();
         myCar1.start();
         myCar1.stop();
-        System.out.println (myCar1.drive(2));
+        //gets result + returns "distance":
+        // System.out.println (myCar1.drive(2));
+        int cargo;
+        cargo = myCar1.drive(2);
+        //returns "distance":
+        // System.out.println(cargo);
     }
 }
