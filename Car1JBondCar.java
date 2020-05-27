01ec62e69441cf1320895dff1bca4d1a57727121
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-05-27 18:41:38 +0300

    new class Car1JBondCar.

---
+++ Car1JBondCar.java
@@ -0,0 +1,9 @@
+public class Car1JBondCar extends Car1 {
+    //Method overriding (suppresing) (p49/p56):
+    int distance = 0;
+    public int drive (int howlong){
+        distance = howlong * 180;
+        System.out.println("With speed 180mp/h car driven " + distance + " miles.");
+        return distance;
+    }  
+}
