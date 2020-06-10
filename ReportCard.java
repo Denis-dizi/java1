29d0fd62d4196b9649b3e01ca40261397dc3471d
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-06-10 13:37:16 +0300

    ReporCard got to work.

--- ReportCard.java
+++ ReportCard.java
@@ -1,12 +1,25 @@
 public class ReportCard {
+        //p/p64-Ru
     public char convertGrades (int testResult) {
-        grade = "A";
-    } else if (testResult >=80 && testResult < 90){
-        grade = "B";
-    } else if (testResult >=70 && testResult < 80){
-        grade = "C";
-    } else {
-        grade = "D";
+            char grade;
+            if (testResult >90) {
+            grade = 'A';
+        } else if (testResult >=80 && testResult < 90){
+            grade = 'B';
+        } else if (testResult >=70 && testResult < 80){
+            grade = 'C';
+        } else {
+            grade = 'D';
+        }
+        return grade;
     }
-    return grade;
-    
+    public static void main(String[] args) {
+        ReportCard rc = new ReportCard();
+        char yourGrade = rc.convertGrades(88);
+        System.out.println("Your first grade " + yourGrade);
+        yourGrade = rc.convertGrades(79);
+        System.out.println("Your second grade " + yourGrade);
+        yourGrade = rc.convertGrades(91);
+        System.out.println("Your third grade " + yourGrade);
+    }
+}
